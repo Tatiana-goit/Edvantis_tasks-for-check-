688cc8aa12a87a0e7145e879c2e96befdbd52b53
Tatsiana  Taranushchenko <tatsiana.taranushchenko@trustradius.com>
2023-01-20 18:18:21 +0300

    fixed task structure

--- Tasks_03/1_data_structure.js
+++ Tasks_03/1_data_structure.js
@@ -6,33 +6,33 @@
 // delete(key)
 // складність всіх методів має дорівнювати O(1)
 
-
-// Задание сделала со 2 попытки как поняла, используя {} в 13 строке. Если не верно, то можно подробнее)
 class DataStructure {
   constructor() {
     this.data = {}
+    this.cloud = ''
+    this.keys = {}
   }
 
   insert(key, value) {
+    this.keys[key] = true;
     this.data[key] = value
   }
 
   find(key) {
+    if (!this.keys[key]) {
+      return `key = ${this.cloud}`
+    }
     const current = this.data[key]
     if(!current) {
-      return 'this key is not here'
+      return `this key ${key} is not here`
     } 
-    return 'this key is here'
+    return `key = ${key} data = ${current}`
   }
 
-  //  Не совсем понятен данный метод setAll, что именно он должен делать?
-  // Сделала так)) но прекрасно вижу сложность  - O(n)
   setAll(value) {
-    const array = value.split(' ')
-    for (let i = 0; i < array.length; i++) {
-      this.data[i] = array[i]
-    }
-    return this.data
+    this.cloud = value
+    this.keys = {}
+    return `Set all "${value}"`;
   }
 
   deleteAll() {
@@ -45,9 +45,8 @@ class DataStructure {
       return 'no such key to delete'
     } 
     delete this.data[key]
-    return 'removal was successful'
+    return `removal ${current} was successful`
   }
-
 }
 
 
@@ -56,14 +55,21 @@ dataStructure.insert(0, 'My');
 dataStructure.insert(1, 'name');
 dataStructure.insert(2, 'is');
 dataStructure.insert(3, 'Tatiana');
-console.log(dataStructure.data);
+console.log('data', dataStructure.data);
 
-console.log(dataStructure.find('3'));
-console.log(dataStructure.find('5'));
+console.log('find', dataStructure.find('3'));
 
-console.log(dataStructure.delete(3));
-console.log(dataStructure.data);
-console.log(dataStructure.deleteAll());
+console.log('delete', dataStructure.delete(3));
+console.log('data', dataStructure.data);
+// console.log('deleteAll', dataStructure.deleteAll());
 
-const dataStructureforSetAll = new DataStructure();
-console.log(dataStructureforSetAll.setAll('My name is Bogdan'));
+console.log(dataStructure.setAll('new info'));
+
+console.log(dataStructure.find('1'));
+dataStructure.insert(3, 'New');
+console.log('data', dataStructure.data);
+console.log('find', dataStructure.find('2'));
+console.log('find', dataStructure.find('3'));
+dataStructure.insert(4, '!');
+console.log('find', dataStructure.find('1'));
+console.log('find', dataStructure.find('4'));
